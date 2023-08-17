799f284182918b4428351485353ccc898f8e5c80
Amir Iqbal <46140044+amiriqbal4@users.noreply.github.com>
2023-08-17 16:04:01 -0400

    Add files via upload

---
+++ Go1_Control_v2/unitree_legged_sdk-master/build/CMakeFiles/example_joystick.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example_joystick.dir/example/example_joystick.cpp.o"
+  "example_joystick.pdb"
+  "example_joystick"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example_joystick.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Go1_Control_v2/unitree_legged_sdk-master/build/CMakeFiles/example_walk.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/amir/ros_catkin_ws/catkin_ws/src/Go1_Control_v2/unitree_legged_sdk-master/example/example_walk.cpp" "/home/amir/ros_catkin_ws/catkin_ws/src/Go1_Control_v2/unitree_legged_sdk-master/build/CMakeFiles/example_walk.dir/example/example_walk.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
